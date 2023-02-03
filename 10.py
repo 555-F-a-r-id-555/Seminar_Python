44ada60a640197f8cf9c9646eafa7bec13b26481
Farid Bagirov <makflay85zet@gmail.com>
2023-02-04 02:23:38 +0400

    intial commit

---
+++ 10.py
@@ -0,0 +1,22 @@
+size = int(input())
+x = [0] * (size)
+
+i = 0
+while i < size:
+    x[i] = int(input())
+    if i < 1:
+        max1 = x[0]
+        min1 = x[0]
+        iMax = 0
+        iMin = 0
+    else:
+        if x[i] > max1:
+            max1 = x[i]
+            iMax = i
+        else:
+            if x[i] < min1:
+                min1 = x[i]
+                iMin = i
+    i = i + 1
+print('iMax =',iMax)
+print('iMin =',iMin)
