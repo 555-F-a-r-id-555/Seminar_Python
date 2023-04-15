60d9311d504d5da4674ab3b80b20473ced11923d
Farid Bagirov <makflay85zet@gmail.com>
2023-04-15 21:46:52 +0400

    added Dop_zadaniye 3,7

--- 03_seminar/Dop_zadaniye.py
+++ 03_seminar/Dop_zadaniye.py
@@ -43,15 +43,60 @@ def func_intersection(l1, l2):
 
 print(func_intersection(list1, list2))
 
-
-
 # 3 Sample Input:
 # ["eat", "tea", "tan", "ate", "nat", “bat"]
 # Sample Output:
 # [["ate", "eat", "tea"], ["nat", "tan"], ["bat"]]
 # Т.е.сгруппировать слова по "общим буквам".
 
+# работает некорректно
+
+lst1 = ['eat', 'tea', 'tan', 'ate']  # , 'nat', 'bat']
+
+lst3 = []
+for i in range(len(lst1)):
+    x1 = lst1[i]
+    lst2 = []
+    for j in range(i, len(lst1)):
+
+        t = True
+        for i in x1:
+            l = len(lst1[j])
+            while l > 0:
+                if i in lst1[j]:
+                    l -= 1
+                else:
+                    t = False
+                    break
+        if t:
+            lst2.append(lst1[j])
+
+    lst3.append(lst2)
+
+print(lst3)
 
 # 7 Слияние отрезков:
 # Вход: [1, 3] [100, 200] [2, 4]
 # Выход: [1, 4] [100, 200]
+
+lst1 = [1, 3]
+lst2 = [100, 200]
+lst3 = [2, 4]
+
+
+lst1 = {i for i in range(lst1[0], lst1[1]+1)}
+lst2 = {i for i in range(lst2[0], lst2[1]+1)}
+lst3 = {i for i in range(lst3[0], lst3[1]+1)}
+
+if lst1 & lst3 != set():
+    x1 = lst1.union(lst3)
+    print(lst_out1 := [min(x1), max(x1)])
+if lst1 & lst2 != set():
+    x2 = lst1.union(lst2)
+    print(lst_out2 := [min(x2), max(x2)])
+if lst2 & lst3 != set():
+    x3 = lst2.union(lst3)
+    print(lst_out3 := [min(x3), max(x3)])
+if lst3 & lst2 == set() and lst1 & lst2 == set():
+    print(lst_out4 := [min(lst2), max(lst2)])
+
